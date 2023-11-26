95a98b73fb5357715959651d5cf24938c9c66c49
uetsymphonique <minhtran6464.lhp@gmail.com>
2023-11-26 20:38:05 +0700

    No.104 Maximum Depth of Binary Tree

---
+++ binary_tree/maximum_depth_of_binary_tree.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def maxDepth(root: Optional[TreeNode]) -> int:
+    if root is None:
+        return 0
+    return 1 + max(maxDepth(root.left), maxDepth(root.right))
